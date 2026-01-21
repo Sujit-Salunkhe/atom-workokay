582036444a118109632f442fb68408fd88360219
Sujit Salunkhe <sujitsalunkhe233@gmail.com>
2026-01-21 12:19:55 +0530

    removing import react-vite error from here

--- src/components/checkbox/Checkbox.stories.tsx
+++ src/components/checkbox/Checkbox.stories.tsx
@@ -1,5 +1,5 @@
 // src/components/ui/checkbox.stories.tsx
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import { useState } from 'react'
 import { Checkbox } from './Checkbox'
 
--- src/components/dialogBox/Dialogbox.stories.tsx
+++ src/components/dialogBox/Dialogbox.stories.tsx
@@ -1,5 +1,5 @@
 // Dialog.stories.tsx
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react-vite'
 import { useState } from 'react'
 import {
   Dialog,
